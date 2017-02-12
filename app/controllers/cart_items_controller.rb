f42e867289f755db641a7904da3227f48eaa6a12
baohua <113983633@qq.com>
2017-02-12 23:16:08 +0800

    add order model

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -3,7 +3,8 @@ class CartItemsController < ApplicationController
 
   def destroy
     @cart = current_cart
-    @cart_item = @cart.cart_items.find_by(product_id: params[:id])
+    @cart_item = @cart.cart_items.find(params[:id])
+binding.pry
     @product = @cart_item.product
     @cart_item.destroy
 
